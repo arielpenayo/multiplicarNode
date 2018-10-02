0777476fdbe5dcf232eee01a8a7cc3e8a7010f8e
ariel_penayo <ariel_penayo@hotmail.com>
2018-10-02 11:17:08 -0400

    commit inicial

---
+++ config/yargs.js
@@ -0,0 +1,21 @@
+//required
+const opts = {
+    base: {
+        demand: true,
+        alias: 'b'
+    },
+    limite: {
+        alias: 'l',
+        default: 10
+    }
+}
+
+const argv = require('yargs')
+    .command('listar', 'imprime en consola la tabla de multiplicar', opts)
+    .command('crear', 'crea el txt de la tabla', opts)
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
